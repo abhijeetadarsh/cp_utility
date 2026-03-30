ec34dea1953a1f2eb579be71d9f478fc5c2718d6
Abhijeet Adarsh <ad.abhijeetadarsh@gmail.com>
2026-03-31 01:38:23 +0530

    initial working setup

---
+++ README.md
@@ -0,0 +1,8 @@
+first install vcpkg, msys2
+then install gcc, cmake, ninja using msys2
+one of these must be set
+
+VCPKG_DEFAULT_HOST_TRIPLET=x64-mingw-dynamic 
+if you want dll in your project
+
+VCPKG_DEFAULT_HOST_TRIPLET=x64-mingw-static 
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv) {
+    CLI::App app{"App description"};
+    argv = app.ensure_utf8(argv);
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
+}
